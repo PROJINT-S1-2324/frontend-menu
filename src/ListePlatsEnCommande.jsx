0d4080cfe591f034776d1a4f12f85cf6a20366f4
kewou.durand <kewoudurand@gmail.com>
2023-12-23 21:23:19 +0100

    Ajout du bouton commande terminer a

--- src/ListePlatsEnCommande.jsx
+++ src/ListePlatsEnCommande.jsx
@@ -35,6 +35,33 @@ const ListePlatsEnCommande = () => {
         chargerPlatsEnCommande();
     }, [isAuthenticated, getAccessTokenSilently]);
 
+    const supprimerCommande = async (commandeId) => {
+        try {
+            if (!isAuthenticated) {
+                console.error('Vous devez être connecté pour supprimer une commande.');
+                return;
+            }
+
+            const accessToken = await getAccessTokenSilently();
+            const response = await fetch(`/api/commandeDelete/${commandeId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                },
+            });
+
+            if (response.ok) {
+                const updatedPlatsEnCommande = platsEnCommande.filter((plat) => plat.id !== commandeId);
+                setPlatsEnCommande(updatedPlatsEnCommande);
+            } else {
+                console.error('Erreur lors de la suppression de la commande depuis le backend:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la suppression de la commande depuis le backend:', error);
+        }
+    };
+
+
     return (
         <div>
             <h2>Plats en Commande</h2>
@@ -52,6 +79,9 @@ const ListePlatsEnCommande = () => {
                         <td>{plat.nomPlat}</td>
                         <td>{plat.prixPlat} €</td>
                         <td>{plat.quantite}</td>
+                        <td>
+                            <button type="button" className="btn btn-danger" onClick={() => supprimerCommande(plat.id)}>Commande Terminer </button>
+                        </td>
                     </tr>
                 ))}
                 </tbody>
