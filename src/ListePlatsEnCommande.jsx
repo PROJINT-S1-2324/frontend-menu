30cd60e1e27fe51e5c787e6144633025d7207b57
kewou.durand <kewoudurand@gmail.com>
2023-12-25 20:35:02 +0100

    Recuperation de id plat pour son nom et prix

--- src/ListePlatsEnCommande.jsx
+++ src/ListePlatsEnCommande.jsx
@@ -5,7 +5,7 @@ const ListePlatsEnCommande = () => {
     const { isAuthenticated, getAccessTokenSilently } = useAuth0();
     const [platsEnCommande, setPlatsEnCommande] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
-    const [platsPerPage] = useState(2);
+    const [platsPerPage] = useState(2); // Nombre de plats par page
 
     useEffect(() => {
         const chargerPlatsEnCommande = async () => {
@@ -44,6 +44,32 @@ const ListePlatsEnCommande = () => {
     const paginate = (pageNumber) => {
         setCurrentPage(pageNumber);
     };
+    const supprimerCommande = async (commandeId) => {
+        try {
+            if (!isAuthenticated) {
+                console.error('Vous devez être connecté pour supprimer une commande.');
+                return;
+            }
+
+            const accessToken = await getAccessTokenSilently();
+            const response = await fetch(`/api/commandeDelete/${commandeId}`, {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                },
+            });
+
+            if (response.ok) {
+                const updatedPlatsEnCommande = platsEnCommande.filter((plat) => plat.id !== commandeId);
+                setPlatsEnCommande(updatedPlatsEnCommande);
+            } else {
+                console.error('Erreur lors de la suppression de la commande depuis le backend:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la suppression de la commande depuis le backend:', error);
+        }
+    };
+
 
     return (
         <div>
@@ -57,11 +83,11 @@ const ListePlatsEnCommande = () => {
                 </tr>
                 </thead>
                 <tbody>
-                {currentPlats.map((plat) => (
-                    <tr key={plat.id}>
-                        <td>{plat.nomPlat}</td>
-                        <td>{plat.prixPlat} €</td>
-                        <td>{plat.quantite}</td>
+                {currentPlats.map((commande) => (
+                    <tr key={commande.id}>
+                        <td>{commande.plat.nom}</td>
+                        <td>{commande.plat.prix} €</td>
+                        <td>{commande.quantite}</td>
                         <td>
                             <button type="button" className="btn btn-danger" onClick={() => supprimerCommande(plat.id)}>Commande Terminée</button>
                         </td>
