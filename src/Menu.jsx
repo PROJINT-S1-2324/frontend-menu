db539974b7ef307be4cec05d28c18a6cfacb32dc
kewou.durand <kewoudurand@gmail.com>
2023-12-30 18:11:24 +0100

    Ajout du boutton list des plats

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import Commande from './Commande';
 import FormulaireAjoutPlat from './FormulaireAjoutPlat';
 import { useAuth0 } from "@auth0/auth0-react";
 import Authentification from './Authentification';
 
+
 const Menu = () => {
     const [activeSection, setActiveSection] = useState(null);
     const [plats, setPlats] = useState([]);
-    const [isAuthenticated, setIsAuthenticated] = useState(false); // Variable d'état pour suivre l'authentification
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const { getAccessTokenSilently } = useAuth0();
 
     const { isAuthenticated: auth0IsAuthenticated } = useAuth0();
 
     const handleSectionClick = (section) => {
         setActiveSection(section);
 
-        // Vérifier si l'utilisateur est authentifié avant d'autoriser l'ajout de plat
+
         if (section === 'Authentification') {
             if (auth0IsAuthenticated) {
                 setIsAuthenticated(true);
@@ -25,9 +27,29 @@ const Menu = () => {
     };
 
     const handleAjouterNouveauPlat = (nouveauPlat) => {
-        // Ajouter le nouveau plat à la liste des plats existants
+
         setPlats([...plats, { id: plats.length + 1, ...nouveauPlat }]);
     };
+    const fetcher = async () => {
+        try {
+            const accessToken = await getAccessTokenSilently();
+            const response = await fetch("/api/plat", {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                },
+            });
+            const data = await response.json();
+            setPlats(data);
+        } catch (error) {
+            console.error("Erreur lors de la récupération des plats depuis le backend:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetcher();
+    }, []);
+
 
     return (
         <div>
@@ -47,26 +69,49 @@ const Menu = () => {
                 >
                     Authentification
                 </button>
+                <button
+                    type="button"
+                    className={`btn ${activeSection === 'ListePlats' ? 'btn-primary' : 'btn-secondary'}`}
+                    onClick={() => {
+                        handleSectionClick('ListePlats');
+                        fetcher();
+                    }}
+                >
+                    Liste des Plats
+                </button>
             </div>
 
             {activeSection && (
                 <div>
-                    {/* Contenu spécifique à chaque section */}
+
                     {activeSection === 'Commande' && (
                         <div>
                             <h2>Contenu de la section Commande</h2>
-                            <Commande plats={plats} /> {/* Passer la liste des plats comme une propriété */}
+                            <Commande plats={plats} />
                         </div>
                     )}
 
                     {activeSection === 'Authentification' && (
                         <div>
                             <h2>Contenu de la section Authentification</h2>
+                            <p>Si vous êtes du personnel, veuillez vous authentifier.</p>
+                            <p>Merci.</p>
+
                             {isAuthenticated ? (
                                 <FormulaireAjoutPlat onAjouterPlat={handleAjouterNouveauPlat} />
                             ) : (
                                 <Authentification />
                             )}
+                        </div>
+                    )}
+                    {activeSection === 'ListePlats' && (
+                        <div>
+                            <h2>Liste des Plats</h2>
+                            <ul>
+                                {plats.map((plat) => (
+                                    <li key={plat.id}>{plat.nom}</li>
+                                ))}
+                            </ul>
                         </div>
                     )}
                 </div>
